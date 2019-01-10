9083b5f9203c442328c723093bcb78716593cb5e
Dragisa Krsmanovic <dragisa.krsmanovic@chartboost.com>
2019-01-10 15:53:36 -0800

    recursion schemes

--- src/main/scala/algos/recursionscheme/Exp.scala
+++ src/main/scala/algos/recursionscheme/Exp.scala
@@ -5,19 +5,22 @@ sealed trait Exp[A]
 
 final case class Const[A](x: Int) extends Exp[A]
 
-final case class Add[A](a: A, x1: Exp[A], x2: Exp[A]) extends Exp[A]
+final case class Add[A](x1: Exp[A], x2: Exp[A]) extends Exp[A]
 
-final case class Mul[A](a: A, x1: Exp[A], x2: Exp[A]) extends Exp[A]
+final case class Mul[A](x1: Exp[A], x2: Exp[A]) extends Exp[A]
 
 object Exp {
 
   implicit val expFunctor: Functor[Exp] = new Functor[Exp] {
     override def map[A, B](fa: Exp[A])(f: A => B): Exp[B] = fa match {
-      case Const(x)       => Const[B](x)
-      case Add(a, x1, x2) => Add[B](f(a), map(x1)(f), map(x2)(f))
-      case Mul(a, x1, x2) => Mul[B](f(a), map(x1)(f), map(x2)(f))
+      case Const(x)    => cons(x)
+      case Add(x1, x2) => add(map(x1)(f), map(x2)(f))
+      case Mul(x1, x2) => mul(map(x1)(f), map(x2)(f))
     }
   }
 
-  val anaExp = ana[Exp, Int](i => Const(i))
+  def cons[A](x: Int): Exp[A]                = Const(x)
+  def add[A](x1: Exp[A], x2: Exp[A]): Exp[A] = Add(x1, x2)
+  def mul[A](x1: Exp[A], x2: Exp[A]): Exp[A] = Mul(x1, x2)
+
 }
