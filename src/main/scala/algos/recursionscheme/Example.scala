0fafedc91efb034bd7ff8705d0ae213381a6943a
Dragisa Krsmanovic <dragisa.krsmanovic@chartboost.com>
2019-01-11 13:17:39 -0800

    List find

--- src/main/scala/algos/recursionscheme/Example.scala
+++ src/main/scala/algos/recursionscheme/Example.scala
@@ -4,4 +4,14 @@ object Example {
 
   val facF = fix[Int => Int](fac => n => if (n == 0) 1 else n * fac(n - 1))
 
+  def findNeg(l: List[Int]): Option[Int] = l match {
+    case Nil    => None
+    case x :: l => if (x > 0) findNeg(l) else Some(x)
+  }
+
+  val findNegF = fix[List[Int] => Option[Int]](f => {
+    case Nil    => None
+    case x :: l => if (x > 0) f(l) else Some(x)
+  })
+
 }
---
+++ src/main/scala/algos/recursionscheme/Stack.scala
@@ -0,0 +1,19 @@
+package algos.recursionscheme
+import cats.Functor
+
+sealed trait Stack[A]
+final case class Done[A](result: Int)     extends Stack[A]
+final case class More[A](a: A, next: Int) extends Stack[A]
+
+object Stack {
+  implicit val stackFunctor: Functor[Stack] = new Functor[Stack] {
+    override def map[A, B](sa: Stack[A])(f: A => B): Stack[B] =
+      sa match {
+        case Done(result)  => Done(result)
+        case More(a, next) => More(f(a), next)
+      }
+  }
+
+  def done[A](result: Int = 1): Stack[A] = Done(result)
+  def more[A](a: A, next: Int): Stack[A] = More(a, next)
+}
